fd0fa4638f684fab46ce34ecb7a53571443292f5
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-19 15:01:39 -0400

    Body section: google input form

--- components/Body.js
+++ components/Body.js
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
+
 function Body() {
-  return <div>Body</div>;
+  return (
+    <div>
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          width="300"
+          height="100"
+          alt="google logo"
+          objectFit="cover"
+        />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-lg lg:max-w-2xl">
+          <SearchIcon className="h-5 text-gray-500 mr-3" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I am Feeling Lucky</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Body;
