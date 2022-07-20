46d7cc903df1eda858be09ee88e52635d279981a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-19 22:03:10 -0400

    create search header component

--- components/User.js
+++ components/User.js
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 
-function User() {
+function User({ className }) {
   const { data: session } = useSession();
 
   if (session) {
@@ -12,7 +12,7 @@ function User() {
             onClick={signOut}
             src={session.user.image}
             alt="user avatar"
-            className="h-10 w-10 rounded-full cursor-pointer hover:bg-gray-200 p-1"
+            className={`h-10 w-10 rounded-full cursor-pointer hover:bg-gray-200 p-1 ${className}`}
           />
         </picture>
       </>
@@ -22,7 +22,7 @@ function User() {
   return (
     <button
       onClick={signIn}
-      className="bg-blue-500 text-white px-5 py-2 font-medium rounded-md hover:brightness-110 hover:shadow-md"
+      className={`bg-blue-500 text-white px-5 py-2 font-medium rounded-md hover:brightness-110 hover:shadow-md ${className}`}
     >
       Sign In
     </button>
