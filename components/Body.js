58c5e1743d70c7fd3038c302e47f47be49e63090
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-19 21:02:05 -0400

    get the term from input and redirect to the related page

--- components/Body.js
+++ components/Body.js
@@ -1,7 +1,22 @@
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 function Body() {
+  const searchInputRef = useRef(null);
+  const router = useRouter();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    //get the search term
+    const term = searchInputRef.current.value;
+    if (!term.trim()) {
+      return;
+    }
+    router.push(`/search?term=${term.trim()}`);
+  };
+
   return (
     <div>
       <form className="flex flex-col items-center mt-40">
@@ -14,11 +29,17 @@ function Body() {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-lg lg:max-w-2xl">
           <SearchIcon className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input
+            type="text"
+            ref={searchInputRef}
+            className="flex-grow focus:outline-none"
+          />
           <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={handleSearch} className="btn">
+            Google Search
+          </button>
           <button className="btn">I am Feeling Lucky</button>
         </div>
       </form>
